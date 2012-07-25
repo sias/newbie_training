3dd5625253b103c242f95e771ffd52bee4f56552
sias <327272367@qq.com>
2012-07-26 05:56:59 +0800

    Modules

---
+++ linux_android_basic_training/linux_drivers_training/ex_01/code/luck.c
@@ -0,0 +1,19 @@
+#include<linux/init.h>
+#include<linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int luck_init(void)
+{
+	printk(KERN_INFO "luck come\n");
+	return 0;
+}
+static void luck_exit(void)
+{
+
+	printk(KERN_INFO "luck exit\n");
+}
+module_init(luck_init);
+module_exit(luck_exit);
+MODULE_AUTHOR("BRUSH ");
+MODULE_DESCRIPTION("A SIMPLE  LUCK WORD MODULE");
+MODULE_ALIAS("A SIMPLE MODULES");
