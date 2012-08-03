ea26e31438a943cc7392c4f7a1f56e83494bd658
sias <327272367@qq.com>
2012-08-03 22:12:10 +0800

    second

--- weekly_programming/ex02/code/one.c
+++ weekly_programming/ex02/code/one.c
@@ -3,23 +3,35 @@ filename:one.c
 purpose:calculat how many ones in a num
 creator:Bruse li
 create time:2012-08-03
+modify history:
+Bruse li,2012-08-03
 */
 #include<stdio.h>
-
-int main(){
-
-	int num;
+/*计算一个整数中一的个数*/
+int ones(int num)
+{
 	int count=0;
-        printf("please enter a number:\n");
-	scanf("%d",&num);
-	int code=num;
-        while(num!=0){
-		if((num & 1) !=0){
+	while(num!=0)
+	{
+		if((num & 1)!=0)
+		{
 			count++;
 		}
 		num=(num>>1);
-        }
-        printf("there are %d one in num %d \n",count,code);   
+	}
+	return count;
+}
+/*主函数*/
+int main()
+{
+	int n;
+	int code;
+	int counter;
+	printf("please enter a number:");
+	scanf("%d",&n);
+	code=n;
+	counter=ones(n);//调用ones()函数
+        printf("There are %d one in num %d \n",counter,code);   
   
    	return 0;
 }
