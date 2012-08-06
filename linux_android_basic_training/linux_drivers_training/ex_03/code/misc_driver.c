0b307d12b6e2f5fa53bcf2571eb3c4701952f9dd
sias <327272367@qq.com>
2012-08-07 02:28:29 +0800

    third

--- linux_android_basic_training/linux_drivers_training/ex_03/code/misc_driver.c
+++ linux_android_basic_training/linux_drivers_training/ex_03/code/misc_driver.c
@@ -1,8 +1,10 @@
 /*
-file name:char_driver5.c
-purpose:char device driver
+file name:misc_driver.c
+purpose:misc device driver
 creator:Bruse li
 create time:2012-08-02
+modify history:
+Bruse li,2012-08-06
 */
 #include<linux/miscdevice.h>
 #include<linux/module.h>
@@ -18,36 +20,36 @@ create time:2012-08-02
 #include<asm/system.h>
 #include<asm/uaccess.h>
 #define RANGE 1024
-#define MISC_NAME  "misc_door"//定义设备名
+#define MISC_NAME  "misc_err"//定义设备名
 
 //设置设备结构体
 struct chardev_dev
 {
-       struct cdev cdev;
-       unsigned char str[RANGE];
+	struct cdev cdev;
+	unsigned char str[RANGE];
 };
 static struct chardev_dev *chardev_devp;
 int chardev_open(struct inode *inode,struct file *filp)
 {
 	filp->private_data=chardev_devp;
-        return 0;
+	return 0;
 }
 int chardev_release(struct inode *inode,struct file *filp)
 {
-        return 0;
+	return 0;
 }
 ssize_t chardev_read(struct file *filp,char *buff,size_t size,loff_t *offp)
 {
 	unsigned long p =*offp;
 	unsigned int count=size;
 	int ret=0;
-	struct chardev_dev *dev =filp->private_data;//houde she bei jie guo ti zhi zhen
+	struct chardev_dev *dev =filp->private_data;//获得设备结构体指针
 	if( p >= RANGE)
 		return 0;
 	if(count>RANGE-p)
 		count = RANGE - p;
 	if(copy_to_user(buff,(void *)(dev->str+p),count)){
-			ret = -EFAULT;
+		ret = -EFAULT;
 		printk(KERN_ALERT "something done\n");
 	}else {
 		*offp +=count;
@@ -77,38 +79,40 @@ ssize_t chardev_write(struct file *filp,const char *buff,size_t size,loff_t *off
 	return ret;
 }
 
-static struct file_operations chardev_fops = {
-
-        .owner = THIS_MODULE,
-        .open = chardev_open,
-        .release  = chardev_release,
-        .read = chardev_read,
-        .write = chardev_write,
+static struct file_operations misc_fops = {
+	.owner = THIS_MODULE,
+	.open = chardev_open,
+	.release  = chardev_release,
+	.read = chardev_read,
+	.write = chardev_write,
 };
 
 static struct miscdevice misc_dev =
-{
-	 .minor = MISC_DYNAMIC_MINOR,//次设备号
-	 .name = MISC_NAME,//设备名
-	 .fops = &chardev_fops,
+{	
+	.minor = MISC_DYNAMIC_MINOR,//次设备号
+	.name = MISC_NAME,//设备名
+	.fops = &misc_fops,
 };
 
 //设备驱动加载模块
 static int chardev_init(void)
 {      
-	 int ret=0;
-	 ret = misc_register(&misc_dev);
-	 if (ret)
-	 {
-		  printk("misc_register error\n");
-	 	  return ret;
-	 }
+	int ret=0;
+	chardev_devp=kmalloc(sizeof(struct chardev_dev),GFP_KERNEL);
+	memset(chardev_devp,0,sizeof(struct chardev_dev));
+	ret = misc_register(&misc_dev);
+	if (ret)
+	{
+		printk("misc_register error\n");
+		return ret;
+	}
  
-	 return 0;
+	return ret;
 }
 static void chardev_exit(void)
-{
-         misc_deregister(&misc_dev);
+{	 
+	kfree(chardev_devp);
+	misc_deregister(&misc_dev);
 }
 module_init(chardev_init);
 module_exit(chardev_exit);
