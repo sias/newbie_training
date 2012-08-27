e57cb97628b372fdf217437678cb33143bdb26e4
sias <327272367@qq.com>
2012-08-28 06:59:01 +0800

    five

--- weekly_programming/ex05/code/dif_num.c
+++ weekly_programming/ex05/code/dif_num.c
@@ -3,28 +3,28 @@ file name:dif_num.c
 purporse:copy the random numbers into a file
 creator:Bruse li
 create time:2012-08-25
+modify history:
+Bruse li,2012-08-27
 */
 #include<stdio.h>
-#include<stdio.h>
-#include<fcntl.h>
-#define N 10000000
-#define K 5000000
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#define N 1000
+#define M 500
 int main()
 {
-	int fd,i,num;
-	if(fd=open("/home/li/Desktop/h.txt",O_RDWR|O_APPEND)==-1)
-	{
-		printf("open file errno!\n");
-	}
-	srand((unsigned)time(NULL));
-	for(i=0;i<K;i++)
-	{
-		num =rand()%N+1;
-		printf("%d\n",num);
-		if(write(fd,&num,sizeof(int))!=4)
+	int i;
+	int m=M;
+	FILE *fp;
+	fp=fopen("file.txt","at+");
+	for(i=0;i<N;i++)
+	{	
+		if((rand()%(N-i))<M)
 		{
-			printf("write errno!\n");
+			fprintf(fp,"%5d",i);
+			m--;	
 		}
 	}
-		close(fd);
+		fclose(fp);
 }
