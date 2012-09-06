5780a1b3d4891b0078237bb3ddd532c3ab908154
sias <327272367@qq.com>
2012-09-06 08:00:34 +0800

    english essay
    Owner: Bruse li
    
    Issue: N/A
    
    Discription: english essay
    
    Root cause: N/A

--- weekly_programming/ex06/code/range_num.c
+++ weekly_programming/ex06/code/range_num.c
@@ -7,20 +7,26 @@ create time:2012-08-30
 #include<stdio.h>
 #include<malloc.h>
 #include<stdlib.h>
-#define N 10000000
-#define K 5000000
+#define SHIFT 5
+#define MASK 0x1F
+#define bit 32
+//#define N 10000000
+//#define K 5000000
+#define N 1000
+#define K 100
 #define LEN N*sizeof(int)
 int main()
 {
+	int a[1+N/bit];
 	int i,num;
 	FILE *fp;
-	int *p;
-	p=(int*)malloc(N*sizeof(int));
-	if(p==NULL)
+	//int *p;
+	//p=(int*)malloc(N*sizeof(int));
+	/*if(p==NULL)
 	{
 		printf("malloc errorn\n");
 		exit(1);
-	}
+	}*/
 	fp=fopen("file.txt","r");
 	if(fp==NULL)
 	{
@@ -29,21 +35,24 @@ int main()
 	}
 	for(i=0;i<N;i++)
 	{
-		*(p+i)=0;
+		a[i>>SHIFT]|=~(1<<(i&MASK));
+		//*(p+i)=0;
 	}
 	for(i=0;i<K;i++)
 	{
 		fscanf(fp,"%d ",&num);
-		*(p+num)=1;
+		a[i>>SHIFT]|=(1<<(i&MASK));
+		//*(p+num)=1;
 	}
 	for(i=0;i<N;i++)
 	{
-		while(*(p+i))
+		//while(*(p+i))
+		while(a[i>>SHIFT])
 		{
 			printf("....%d....\n",i);
 			break;
 		}	
 	}
-	free(p);
+	//free(p);
 	fclose(fp);
 }
