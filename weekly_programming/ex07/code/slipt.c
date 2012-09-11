a8ea769dc2bd367ab82b4e516c77972ab0b094e1
sias <327272367@qq.com>
2012-09-12 07:13:50 +0800

    first commit Bruse20120911.txt
    Owner: Bruse li
    
    Issue: N/A
    
    Discription: first commit Bruse20120911.txt
    
    Root cause: N/A

--- weekly_programming/ex07/code/slipt.c
+++ weekly_programming/ex07/code/slipt.c
@@ -8,12 +8,12 @@ create time:2012-09-8
 #include<stdlib.h>
 #include<math.h>
 #include<time.h>
-#define NUM_SIZE 10000
-#define RANGE_NUM rand()%9999+1
+#define NUM_SIZE 10
+#define RANGE_NUM rand()%10
 int i=0;
 int j;
 int num[NUM_SIZE];
-int swap(int a,int b)
+void swap(int a,int b)
 {
 	int temp;
 	temp=a;
@@ -52,17 +52,21 @@ int split(int num[],int low,int high)
 	int x=num[low];
 	while(i<=j)
 	{
+		printf("in split 1 while circle\n");
 		while(num[i]<=x)
 		{
+			printf("in while 1\n");
 			i++;
 		}
 		while(num[j]>x)
 		{
+			printf("in while 2\n");
 			j--;
 		}
 		if(i<j)
 		{
-		swap(num[i],num[j]);
+			printf("in while 3\n");
+			swap(num[i],num[j]);
 		}
 	}
 	swap(num[low],num[j]);
@@ -74,7 +78,10 @@ int quick_sort(int num[],int low,int high)
 	if(low<high)
 	{
 		k=split(num,low,high);
+		printf("befor the first qiuck_sort\n");
 		quick_sort(num,low,k-1);
+		
+		printf("befor the secend qiuck_sort\n");
 		quick_sort(num,k+1,high);
 	}
 	return 0;
@@ -82,19 +89,25 @@ int quick_sort(int num[],int low,int high)
 void main()
 {	
 	clock_t begin1,end1,begin2,end2,begin3,end3;
-	int num[NUM_SIZE];
 	int high,low;
 	low=0;
 	high=NUM_SIZE-1;
 	begin1=clock();
-	dif_num();
-	quik_sort(num,low,high);
+	printf("before dif_num\n");
+	sam_num();
+	for(i=0;i<100;i++)
+	{
+		printf("%d\n",num[i]);
+	}
+	printf("before quick_sort\n");
+	quick_sort(num,low,high);
 	printf("dif_num range time:\n");
 	for(i=0;i<NUM_SIZE;i++)
 	{
 		printf("%4d",num[i]);
 	}
-	end1=clock();
+
+/*	end1=clock();
 	printf("%lf second\n",(double)(end1-begin1)/CLOCKS_PER_SEC);
 	begin2=clock();
 	same_num();
@@ -115,4 +128,5 @@ void main()
 	}
 	end3=clock();	
 	printf("%lf second\n",(double)(end3-begin3)/CLOCKS_PER_SEC);
+	*/
 }
