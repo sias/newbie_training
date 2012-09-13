f13f519262a54c4724e9b947d9b842e46252679e
sias <327272367@qq.com>
2012-09-14 00:14:35 +0800

    updata slipt.c
    Owner: Bruse li
    
    Issue: wrong running
    
    Discription: updata slipt.c
    
    Root cause: wrong running

--- weekly_programming/ex07/code/slipt.c
+++ weekly_programming/ex07/code/slipt.c
@@ -8,29 +8,27 @@ create time:2012-09-8
 #include<stdlib.h>
 #include<math.h>
 #include<time.h>
-#define NUM_SIZE 10
-#define RANGE_NUM rand()%10
+#define NUM_SIZE 10000
+#define RANGE_NUM rand()%10000
 int i=0;
-int j;
-int num[NUM_SIZE];
-void swap(int &a,int &b)
+int x;
+void swap(int *a,int *b)
 {
-	int *temp;
-	temp=&a;
-	&a=&b;
-	&b=temp;
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
 }
-void dif_num()
+void dif_num(int num[])
 {
-	//srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));
 	for(i=0;i<NUM_SIZE;i++)
 	{
 		num[i]=RANGE_NUM;
 	}
 }
-void sam_num()
+void sam_num(int num[])
 {
-	int x;
 	srand((unsigned)time(NULL));
 	x=RANGE_NUM;
 	for(i=0;i<NUM_SIZE;i++)
@@ -38,7 +36,7 @@ void sam_num()
 		num[i]=x;
 	}
 }
-void inc_num()
+void inc_num(int num[])
 {
 	for(i=0;i<NUM_SIZE;i++)
 	{
@@ -52,85 +50,72 @@ int split(int num[],int low,int high)
 	int x=num[low];
 	while(i<=j)
 	{	
-		printf("..%d..%d",num[i],x);
-		printf("in split 1 while circle\n");
 		while(num[i]<=x)
 		{
-			printf("in while 1\n");
 			i++;
-			sleep(3);
-			printf("......%d...",i);
 		}
 		while(num[j]>x)
 		{
-			printf("in while 2\n");
-				sleep(3);
 			j--;
 		}
 		if(i<j)
 		{
-			printf("in while 3\n");
 			swap(&num[i],&num[j]);
 		}
 	}
 	swap(&num[low],&num[j]);
 	return j;
 }
-int quick_sort(int num[],int low,int high)
+int quick_sort(int n[],int low,int high)
 {
 	int k;
 	if(low<high)
 	{
-		k=split(num,low,high);
-		printf("befor the first qiuck_sort\n");
-		quick_sort(num,low,k-1);
-		
-		printf("befor the secend qiuck_sort\n");
-		quick_sort(num,k+1,high);
+		k=split(n,low,high);
+		quick_sort(n,low,k-1);
+		quick_sort(n,k+1,high);
 	}
 	return 0;
 }
 void main()
 {	
+	FILE *fp1,*fp2,*fp3;
 	clock_t begin1,end1,begin2,end2,begin3,end3;
+	int n[NUM_SIZE];
 	int high,low;
 	low=0;
 	high=NUM_SIZE-1;
+	fp1=fopen("dif.txt","w+");
+	fp2=fopen("sam.txt","w+");
+	fp3=fopen("inc.txt","w+");
+	//range difrent numbers 
 	begin1=clock();
-	printf("before dif_num\n");
-	dif_num();
-	for(i=0;i<10;i++)
-	{
-		printf("%d\n",num[i]);
-	}
-	printf("before quick_sort\n");
-	quick_sort(num,low,high);
-	printf("dif_num range time:\n");
+	dif_num(n);
+	quick_sort(n,low,high);
 	for(i=0;i<NUM_SIZE;i++)
 	{
-		printf("%4d",num[i]);
+		fprintf(fp1,"%d ",n[i]);
 	}
-
-/*	end1=clock();
-	printf("%lf second\n",(double)(end1-begin1)/CLOCKS_PER_SEC);
+	end1=clock();
+	printf("\ndif_num range time is: %lf second\n",(double)(end1-begin1)/CLOCKS_PER_SEC);
+	//range same number
 	begin2=clock();
-	same_num();
-	qiuk_sort(num,low,high);
-	printf("same_num range time:\n");
+	sam_num(n);
+	quick_sort(n,low,high);
 	for(i=0;i<NUM_SIZE;i++)
 	{	
-		printf("%4d",num[i]);
+		fprintf(fp2,"%d ",n[i]);
 	}
 	end2=clock();
-	printf("%lf second\n",(double)(end2-begin2)/CLOCKS_PER_SEC);
+	printf("\nsam_num range time is: %lf second\n",(double)(end2-begin2)/CLOCKS_PER_SEC);
+	//range increas number
 	begin3=clock();
-	inc_Num();
-	quick_sort(num,low,high);
+	inc_num(n);
+	quick_sort(n,low,high);
 	for(i=0;i<NUM_SIZE;i++)
 	{
-		printf("%4d",num[i]);
+		fprintf(fp3,"%d ",n[i]);
 	}
 	end3=clock();	
-	printf("%lf second\n",(double)(end3-begin3)/CLOCKS_PER_SEC);
-	*/
+	printf("\ninc_num range time is: %lf second\n",(double)(end3-begin3)/CLOCKS_PER_SEC);	
 }
